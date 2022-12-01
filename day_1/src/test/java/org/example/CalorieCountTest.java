4c9122696b00547976d32540ef7b4b14a00e6c47
Neel Doshi <neel.doshi@sky.uk>
2022-12-01 22:50:25 +0000

    add tests
    Co-authored-by: Anadibya Rai <anadibya.rai@sky.uk>

---
+++ day_1/src/test/java/org/example/CalorieCountTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.model.Elf;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalorieCountTest {
+
+    @Test
+    void calculateTotalCaloriesForElf() {
+        int result = CalorieCount.calculateTotalCaloriesForElf(List.of(1,2));
+        assertEquals(3, result);
+    }
+
+    @Test
+    void getElfWithMostCalories() {
+        Elf elf = new Elf(1, 1);
+        Elf elf2 = new Elf(2, 3);
+        Elf result = CalorieCount.getElfWithMostCalories(List.of(elf, elf2));
+        assertEquals(elf2, result);
+
+    }
+}
